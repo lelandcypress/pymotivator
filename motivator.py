9c5a85d3cdd6e1e7b5657ec70ca5880f25571e25
Michael Souter <souterm1@gmail.com>
2022-04-16 08:44:04 -0400

    made some slight modifications, employed DRY, converted lists to tuples

--- motivator.py
+++ motivator.py
@@ -1,17 +1,17 @@
+import sys
 from selector import *
 import random
-training_goals = ["Python", "C#", "AWS", "Cyber", "React"]
+training_goals = ("Python", "C#", "AWS", "Cyber", "React")
+training_methods_py = ("Free Code Camp", "Udemy",
+                       "CodeCademy", 'Start Coding', "Write Hokey-Pokeys script")
+training_methods_C = ("CodeCademy")
+aws_training = ("Free Code Camp Practinioner",
+                "AWS Game", "Udemy Solutions Architect")
+cyber_training = ("Read the book", "Udemy")
+react_training = ("Udemy", "Building your App")
 
-training_methods_py = ["Free Code Camp", "Udemy",
-                       "CodeCademy", 'Start Coding', "Write Hokey-Pokeys script"]
-training_methods_C = ["CodeCademy"]
-aws_training = ["Free Code Camp Practinioner",
-                "AWS Game", "Udemy Solutions Architect"]
-cyber_training = ["Read the book", "Udemy"]
-react_training = ["Udemy", "Building your App"]
-
-motivation = ["You Got This", "Put in the time",
-              "Give me 1 hour", "Give me 2 hours", "No more excuses"]
+motivation = ("You Got This", "Put in the time",
+              "Give me 1 hour", "Give me 2 hours", "No more excuses")
 chosen_training = random.choice(training_goals)
 
 
@@ -28,6 +28,6 @@ def motivateMe():
     elif chosen_training == "React":
         selector(react_training)
     print(random.choice(motivation))
-    
+
 
 motivateMe()
