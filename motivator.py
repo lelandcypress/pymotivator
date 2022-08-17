bd0c8c5ec3485fb22ebe29ef575b490b8971ec6c
Michael Souter <souterm1@gmail.com>
2022-08-16 21:31:42 -0400

    app now sends message to my phone on init.

--- motivator.py
+++ motivator.py
@@ -3,36 +3,31 @@ from selector import *
 import random
 training_goals = ("Python", "C#", "Java", "AWS", "Cyber", "React",
                   "PeopleSoft", "AWS",)
-training_methods_py = ("Udemy", "CodeCademy", 'Start Coding',
-                       "Write Hokey-Pokeys script", "Open source")
-training_methods_C = ("CodeCademy", "Pick a project")
-training_methods_Java = ("CodeCademy", "Pick a project")
-training_methods_PeopleSoft = ("Milton Book",)
-aws_training = ("Udemy Solutions Architect",)
-cyber_training = ("Read the book", "Udemy")
-react_training = ("Udemy", "Goal Tracker Front End")
-motivation = ("You Got This", "Put in the time",
-              "Give me 1 hour", "Give me 2 hours", "No more excuses")
+slogan = ("You Got This", "Put in the time",
+          "Give me 1 hour", "Give me 2 hours", "No more excuses")
 chosen_training = random.choice(training_goals)
+motivation = random.choice(slogan)
 
 
 def motivateMe():
-    print("Work on", chosen_training)
+
     if chosen_training == "Python":
-        selector(training_methods_py)
+        training = selector(training_methods_py)
     elif chosen_training == "C#":
-        selector(training_methods_C)
+        training = selector(training_methods_C)
     elif chosen_training == "Java":
-        selector(training_methods_Java)
+        training = selector(training_methods_Java)
     elif chosen_training == "PeopleSoft":
-        selector(training_methods_PeopleSoft)
+        training = selector(training_methods_PeopleSoft)
     elif chosen_training == "AWS":
-        selector(aws_training)
+        training = selector(aws_training)
     elif chosen_training == "Cyber":
-        selector(cyber_training)
+        training = selector(cyber_training)
     elif chosen_training == "React":
-        selector(react_training)
-    print(random.choice(motivation))
+        training = selector(react_training)
+    return training
 
 
-motivateMe()
+train = motivateMe
+
+message_to_self = f"Hey Dude! Start the week off right. Focus your efforts on {chosen_training}! Specifically {train()}! Remember {motivation}!"
--- selector.py
+++ selector.py
@@ -1,15 +1,14 @@
 import random
-training_methods_py = ["Free Code Camp", "Udemy",
-                       "CodeCademy", 'Start Coding', "Write Hokey-Pokeys script"]
-training_methods_C = ["CodeCademy"]
-aws_training = ["Free Code Camp Practinioner",
-                "AWS Game", "Udemy Solutions Architect"]
-cyber_training = ["Read the book", "Udemy"]
-react_training = ["Udemy", "Building your App"]
+training_methods_py = ("Udemy", "CodeCademy",
+                       "F-3", "Open source")
+training_methods_C = ("CodeCademy",)
+training_methods_Java = ("CodeCademy",)
+training_methods_PeopleSoft = ("Milton Book",)
+aws_training = ("Udemy Solutions Architect",)
+cyber_training = ("Read the book", "Security+")
+react_training = ("Udemy", "Goal Tracker Front End")
 
 
 def selector(list):
     selected_method = random.choice(list)
-    print(selected_method)
-
-
+    return(selected_method)
